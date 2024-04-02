ed3c7d35f72d224cacfbd1c17524875b0d605f96
Frank Ezene <ezenefrankc@gmail.com>
2024-04-02 17:19:17 +0100

    update image-utils.jsx

--- src/utils/image-utils.jsx
+++ src/utils/image-utils.jsx
@@ -1,11 +1,16 @@
+import clsx from "clsx";
+
 export function getImageUrl(name) {
   return new URL(`${name}`, import.meta.url);
 }
 
-export const getImage = (image, name) => {
+export const getImage = (image, name, isHidden = false) => {
   return (
     <img
-      className="h-8 w-8 rounded-full hidden lg:block"
+      className={clsx(
+        "h-8 w-8 rounded-full md:block",
+        isHidden ? "hidden" : ""
+      )}
       src={getImageUrl(image)}
       alt={`${name}'s avatar`}
     />
