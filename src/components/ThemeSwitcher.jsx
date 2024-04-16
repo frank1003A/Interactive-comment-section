ce5b34b2a94fbc2210762b5a7e1f6fe0cdfab58a
Frank Ezene <ezenefrankc@gmail.com>
2024-04-17 00:39:00 +0100

    update ThemeSwitcher.jsx

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -5,6 +5,7 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 // eslint-disable-next-line react/prop-types
 const ThemeSwitcher = ({ position }) => {
   const [darkMode, setDarkMode] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const isDarkMode = localStorage.getItem("darkMode") === "true";
@@ -20,13 +21,30 @@ const ThemeSwitcher = ({ position }) => {
     setDarkMode(!darkMode);
   };
 
+  useEffect(() => {
+    const inputboxes = document.querySelectorAll("textarea, [role='textbox']");
+
+    inputboxes.forEach((input) => {
+      input.addEventListener("focusin", () => setIsVisible(false));
+      input.addEventListener("focusout", () => setIsVisible(true));
+    });
+
+    return () => {
+      inputboxes.forEach((input) => {
+        input.removeEventListener("focusin", () => setIsVisible(false));
+        input.removeEventListener("focusout", () => setIsVisible(true));
+      });
+    };
+  });
+
   return (
     <button
       role="button"
       onClick={toggleDarkMode}
       className={clsx(
-        " bg-indigo-700 dark:bg-yellow-500 p-2 rounded-md shadow-lg",
-        position === "fixed" || !position ? "fixed bottom-10 left-8 z-50" : ""
+        " bg-indigo-700 dark:bg-yellow-500 p-2 rounded-md shadow-lg transition-all",
+        position === "fixed" || !position ? "fixed bottom-10 left-8 z-50" : "",
+        isVisible ? "-translate-y-[8px]" : "translate-y-[500px]"
       )}
     >
       <DarkModeSwitch
